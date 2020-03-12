8975af55528456b41316e1695e584e0a362bf489
nanduvbabu <62115001+nanduvbabu@users.noreply.github.com>
2020-03-13 00:19:27 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,13 @@
+/* To find the simple interst */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int p,n,r,si;
+    clrscr();
+    printf("Enter Principle, Rate of interest & Time :");
+    scanf("%d%d%d",&p,&r,&n);
+    si=(p*r*n)/100;
+    printf("Simple Interest is :%d",si);
+    getch();
+}
