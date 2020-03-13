60b4d9138051b4461fcef05ba97014b4de37e8ea
nanduvbabu <62115001+nanduvbabu@users.noreply.github.com>
2020-03-13 13:22:30 +0530

    To find the sum of N natural numbers

---
+++ 10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+	{
+		int n,sum=0;
+
+		printf("\n Enter the number of natural numbers (n): ");
+		scanf("%d", &n);
+
+		for(int i=0;i<n;i++)
+			{
+				sum+=i;
+			}
+
+		printf(" \n Sum = %d", sum);
+
+	}
